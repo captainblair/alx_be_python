b7312198818bbf50366a5b807c326a213daa8781
Engineer Tony <wangolotony4@gmail.com>
2025-05-22 08:21:33 +0300

    Fixed calculations

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,15 +1,13 @@
-#Monthly income 
-
+# Monthly income
 monthly_income = int(input("Enter your monthly income: "))
 total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-#Arithmetic operations
+# Arithmetic operations
 monthly_savings = monthly_income - total_monthly_expenses
+annual_savings = monthly_savings * 12
+interest = annual_savings * 0.05
+projected_savings = annual_savings + interest
 
-#Projection
-projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-
-#output results
-print(f"your monthly savings are ${monthly_savings}.")
+# Output results
+print(f"Your monthly savings are ${monthly_savings}.")
 print(f"Projected savings after one year, with interest is: ${int(projected_savings)}.")
-
